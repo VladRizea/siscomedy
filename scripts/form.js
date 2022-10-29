c2cd016960cf2b0e7eb30bf4791ace3e5ce0ed6d
VladRizea <vlad.rizea.stefan@gmail.com>
2022-10-30 00:48:58 +0300

    intro and form

---
+++ scripts/form.js
@@ -0,0 +1,22 @@
+document.querySelector('#regbutton').addEventListener("click", (e) => {
+    e.preventDefault();
+    toastr.remove()
+    const rezervare = {
+        nume: document.querySelector('#nume').value,
+        prenume: document.querySelector('#prenume').value,
+        telefon: document.querySelector('#telefon').value,
+        email: document.querySelector('#email').value,
+    }
+    axios.post('/inscriere', rezervare)
+        .then((response) => {
+            toastr.success("Te-ai inscris cu succes!");
+        })
+        .catch((error) => {
+            const values = Object.values(error.response.data)
+            console.log(error);
+            values.map(item => {
+                toastr.error(item)
+            })
+        })
+
+}, false)
